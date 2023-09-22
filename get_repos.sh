607bbcd1e41293fe4f2b7a684ee9224d3d544d74
Brian Cherinka <havok2063@hotmail.com>
2023-09-22 18:29:08 -0400

    updating files

--- get_repos.sh
+++ get_repos.sh
@@ -1,2 +1,23 @@
-git clone https://github.com/sdss/valis.git valis/valis
-git clone https://github.com/sdss/zora.git zora/zora
+#!/bin/bash
+
+update=0;
+while getopts ":u" flag; do
+ case $flag in
+   u) # Handle the update flag
+   update=1
+   ;;
+ esac
+done
+
+if [[ $update -eq 1 ]]
+then
+ cd valis/valis
+ git pull
+ cd ../../
+ cd zora/zora
+ git pull
+ cd ../../
+else
+ git clone https://github.com/sdss/valis.git valis/valis
+ git clone https://github.com/sdss/zora.git zora/zora
+fi
